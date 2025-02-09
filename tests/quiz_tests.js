3d1b800af3b940218aa837e9007a678d52392e97
Carl Bondin <carl.bondin@gmail.com>
2025-02-09 15:50:27 +0100

    Setup playwright + added a few tests

---
+++ tests/quiz_tests.js
@@ -0,0 +1,46 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://carlbondin.github.io/Quiz/');
+});
+
+test('Quiz is shown on load', async ({ page }) => {
+    await page.getByRole('heading', { name: 'Sports Quiz' }).click();
+});
+
+test.describe('Quiz submissions scores', () => {
+    test('Submission of a perfect score should show perfect result and modal', async ({page}) => {
+        await page.getByText('France').click();
+        await page.getByText('11').click();
+        await page.getByText('Soccer', {exact: true}).click();
+        await page.getByText('USA', {exact: true}).click();
+        await page.getByText('Novak Djokovic').click();
+        await page.getByText('1', {exact: true}).click();
+        await page.getByText('New England Patriots').click();
+        await page.getByText('5', {exact: true}).click();
+        await page.getByText('Usain Bolt').click();
+        await page.getByText('The Stanley Cup').click();
+        await page.getByRole('button', {name: 'Submit'}).click();
+
+        await expect(page.getByRole('heading', {name: 'Quiz Results'})).toBeVisible();
+        await expect(page.getByText('You got 10 out of 10 correct!')).toBeVisible();
+    });
+
+    test('Submission of a 8/10 score should show corresponding result and modal', async ({ page }) => {
+        await page.getByText('Germany').click();
+        await page.getByText('9').click();
+        await page.getByText('Soccer', { exact: true }).click();
+        await page.getByText('USA', { exact: true }).click();
+        await page.getByText('Novak Djokovic').click();
+        await page.getByText('1', { exact: true }).click();
+        await page.getByText('New England Patriots').click();
+        await page.getByText('5', { exact: true }).click();
+        await page.getByText('Usain Bolt').click();
+        await page.getByText('The Stanley Cup').click();
+        await page.getByRole('button', { name: 'Submit' }).click();
+
+        await expect(page.getByRole('heading', { name: 'Quiz Results' })).toBeVisible();
+        await expect(page.getByText('You got 8 out of 10 correct!')).toBeVisible();
+    });
+});
